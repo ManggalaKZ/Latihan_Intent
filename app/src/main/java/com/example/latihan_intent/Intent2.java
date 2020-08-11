6fd5f32f6715a28a7bf3829e96958b06e99b6283
ManggalaKZ <kagendra147@gmail.com>
2020-08-11 13:19:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan_intent/Intent2.java
@@ -0,0 +1,20 @@
+package com.example.latihan_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Intent2 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent2);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/latihan_intent/Intent3.java
@@ -0,0 +1,14 @@
+package com.example.latihan_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intent3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent3);
+    }
+}
